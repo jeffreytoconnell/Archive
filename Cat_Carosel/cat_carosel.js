f0c5ebda03b81cd9f36161433b0c8ba818125843
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-03-11 17:36:03 -0800

    initial

---
+++ Cat_Carosel/cat_carosel.js
@@ -0,0 +1,13 @@
+
+
+function handleThumbnailClicks() {
+  $('.thumbnail').click(function(event) {
+    var imgSrc = $(event.currentTarget).find('img').attr('src'); // Changes imgSrc to target image
+    $('.hero img').attr('src', imgSrc); // Changes div .hero to target above
+  })
+}
+
+$(function() {
+  handleThumbnailClicks();
+});
+
---
+++ Lighbulb/lightbulb.js
@@ -0,0 +1,12 @@
+    
+        function handleBulbClicks() {
+            $('.lightbulb').click(function (event) {
+                $('.lightbulb').removeClass('bulb-on');
+                $(event.currentTarget).addClass('bulb-on')
+            });
+        }
+
+        $(function () {
+            handleBulbClicks();
+        });
+ 
