c6a14628b479a0cd5221602de778521fa35f515a
Jeff O'Connell <Jeff O'Connell>
2017-03-19 12:42:42 -0700

    v1

--- Shopping List/app.js
+++ Shopping List/app.js
@@ -1,12 +1,24 @@
-// **WATCH FOR FORM SUBMISSION**//
-$(function () {
-  addNewItem();
+$(document).ready(function() {
+
+
+  //adding a new item
+  $('button[type="submit"]').click(function() {
+    var groceryItem = $('input#shopping-list-entry').val();
+    $('ul.shopping-list').prepend('<li><span class="shopping-item">'+groceryItem+'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div>');
+  });
+
+  //modifying / checking off
+  $('body').on('click', 'button.shopping-item-toggle', function(){
+    $(this).parent('.shopping-item-controls').siblings('span.shopping-item').toggleClass('lineThrough');
+  });
+
+  // deleting an item
+ $('body').on('click', 'button.shopping-item-delete', function(){
+    $(this).closest('li').remove();
+  });
+
 });
 
-function addNewItem() {
-  $('#js-shopping-list-form').submit(function (event) { //ID NOT CLASS
-    event.preventDefault();
-    var text = $('#shopping-list-entry').val(); //CLASS
-    console.log(text);
-  });
-}
+
+
+
