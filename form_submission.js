7ea5c106977d44534c9865c4cf26bb4397404b26
Jeff O'Connell <Jeff O'Connell>
2017-03-19 12:45:07 -0700

    v1

---
+++ form_submission.js
@@ -0,0 +1,12 @@
++// **WATCH FOR FORM SUBMISSION**//
+ +$(function () {
+ +  addNewItem();
+ +});
+ +
+ +function addNewItem() {
+ +  $('#js-shopping-list-form').submit(function (event) { //ID NOT CLASS
+ +    event.preventDefault();
+ +    var text = $('#shopping-list-entry').val(); //CLASS
+ +    console.log(text);
+ +  });
+ +} 
