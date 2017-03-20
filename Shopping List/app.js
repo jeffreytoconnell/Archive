a7bb9d4e0fc1a6ba27509c00420e5a4a71df7754
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-03-20 16:23:47 -0700

    deleting itself

--- Shopping List/app.js
+++ Shopping List/app.js
@@ -1,24 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
   //adding a new item
-  $('button[type="submit"]').click(function() {
+ $('button[type="submit"]').click(function() {
     var groceryItem = $('input#shopping-list-entry').val();
     $('ul.shopping-list').prepend('<li><span class="shopping-item">'+groceryItem+'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div>');
   });
 
   //modifying / checking off
-  $('body').on('click', 'button.shopping-item-toggle', function(){
+  $('body').on('click', 'button.shopping-item-toggle', function () {
     $(this).parent('.shopping-item-controls').siblings('span.shopping-item').toggleClass('lineThrough');
   });
 
   // deleting an item
- $('body').on('click', 'button.shopping-item-delete', function(){
+  $('body').on('click', 'button.shopping-item-delete', function () {
     $(this).closest('li').remove();
   });
 
-});
-
-
-
-
+})
