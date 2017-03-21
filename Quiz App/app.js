678731f93726ce496180980242065979aaf74df1
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-03-20 19:38:43 -0700

    added click events

--- Quiz App/app.js
+++ Quiz App/app.js
@@ -1,20 +1,15 @@
-$(document).ready(function () {
+$( "#wrong" ).click(function() {
+  alert( "Wrong!!!" );
+});
 
+$( "#correct" ).click(function() {
+  alert( "Wrong!!!" );
+});
 
-  //adding a new item
- $('button[type="submit"]').click(function() {
-    var groceryItem = $('input#shopping-list-entry').val();
-    $('ul.shopping-list').prepend('<li><span class="shopping-item">'+groceryItem+'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div>');
-  });
+$( ".wrong" ).click(function() {
+  alert( ".wrong" );
+});
 
-  //modifying / checking off
-  $('body').on('click', 'button.shopping-item-toggle', function () {
-    $(this).parent('.shopping-item-controls').siblings('span.shopping-item').toggleClass('lineThrough');
-  });
-
-  // deleting an item
-  $('body').on('click', 'button.shopping-item-delete', function () {
-    $(this).closest('li').remove();
-  });
-
+$( ".correct" ).click(function() {
+  alert( ".correct" );
 });
